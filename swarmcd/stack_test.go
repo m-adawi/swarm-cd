aaab2c8e8837421b31d2bcfe38ee71257072027c
Andrea Ghensi <aghensi@systra.com>
2024-09-25 09:13:22 +0200

    fix: ignore external secrets in discovery and rotation
    fixes #53

---
+++ swarmcd/stack_test.go
@@ -0,0 +1,50 @@
+package swarmcd
+
+import (
+	"sync"
+	"testing"
+)
+
+// External objects are ignored by the rotation
+func TestRotateExternalObjects(t *testing.T) {
+	repo := &stackRepo{name: "test", path: "test", url: "", auth: nil, lock: &sync.Mutex{}, gitRepoObject: nil}
+	stack := newSwarmStack("test", repo, "main", "docker-compose.yaml", nil, "", false)
+	objects := map[string]any{
+		"my-secret": map[string]any{"external": true},
+	}
+	err := stack.rotateObjects(objects)
+	if err != nil {
+		t.Errorf("unexpected error: %s", err)
+	}
+}
+
+// Secrets are discovered, external secrets are ignored
+func TestSecretDiscovery(t *testing.T) {
+	repo := &stackRepo{name: "test", path: "test", url: "", auth: nil, lock: &sync.Mutex{}, gitRepoObject: nil}
+	stack := newSwarmStack("test", repo, "main", "stacks/docker-compose.yaml", nil, "", false)
+	stackString := []byte(`services:
+  my-service:
+    image: my-image
+    secrets:
+      - my-secret
+      - my-external-secret
+secrets:
+  my-secret:
+    file: secrets/secret.yaml
+  my-external-secret:
+    external: true`)
+	composeMap, err := stack.parseStackString(stackString)
+	if err != nil {
+		t.Errorf("unexpected error: %s", err)
+	}
+	sopsFiles, err := discoverSecrets(composeMap, stack.composePath)
+	if err != nil {
+		t.Errorf("unexpected error: %s", err)
+	}
+	if len(sopsFiles) != 1 {
+		t.Errorf("unexpected number of sops files: %d", len(sopsFiles))
+	}
+	if sopsFiles[0] != "stacks/secrets/secret.yaml" {
+		t.Errorf("unexpected sops file: %s", sopsFiles[0])
+	}
+}
