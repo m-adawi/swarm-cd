990a1e4b22e51b65c26cbb645ec7c7443b18dbbd
Valentin Haudiquet <valentin.haudiquet@canonical.com>
2025-10-04 16:27:57 +0200

    feat: import gpg key from file
    Signed-off-by: Valentin Haudiquet <vhaudiquet343@hotmail.fr>

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,15 +1,25 @@
 #!/bin/sh
 # Swarm-CD entrypoint, used to import gpg keys if needed
-if [ ! -z "${SOPS_GPG_PRIVATE_KEY}" ]; then
-    echo "entrypoint.sh: importing gpg private key..."
+if [ ! -z "${SOPS_GPG_PRIVATE_KEY_FILE}" ]; then
+    echo "entrypoint.sh: importing gpg private key from file..."
+
+    cat "${SOPS_GPG_PRIVATE_KEY_FILE}" | gpg --import
+
+    if [ $? -ne 0 ]; then
+        echo "entrypoint.sh: error: could not import GPG private key from file !"
+        exit 1
+    fi
+    echo "entrypoint.sh: gpg key imported from file successfully."
+elif [ ! -z "${SOPS_GPG_PRIVATE_KEY}" ]; then
+    echo "entrypoint.sh: importing gpg private key from environment..."
 
     echo "${SOPS_GPG_PRIVATE_KEY}" | gpg --import
     
     if [ $? -ne 0 ]; then
-        echo "entrypoint.sh: error: could not import GPG private key !"
+        echo "entrypoint.sh: error: could not import GPG private key from environment !"
         exit 1
     fi
-    echo "entrypoint.sh: gpg key imported successfully."
+    echo "entrypoint.sh: gpg key imported from environment successfully."
 else
     echo "entrypoint.sh: no gpg key found, skipping import"
 fi
