ab04256ddf0e8984e20b0bcfbe8564756e3b65c1
sanzoghenzo <andrea.ghensi@gmail.com>
2026-02-08 17:12:19 +0100

    fix: limit concurrent deployments with configurable worker pool
    - Add concurrency config option (default: 3 workers)
    - Implement worker pool pattern for parallel stack deployments
    - Workers pull from channel and process stacks concurrently
    - Maintains thread safety with existing repo locks

--- swarmcd/swarmcd.go
+++ swarmcd/swarmcd.go
@@ -6,31 +6,51 @@ import (
 	"time"
 )
 
-var stackStatus map[string]*StackStatus = map[string]*StackStatus{}
+var stackStatus = map[string]*StackStatus{}
 var stacks []*swarmStack
 
 func Run() {
 	logger.Info("starting SwarmCD")
 	for {
+		logger.Debug("starting update loop")
 		var waitGroup sync.WaitGroup
-		logger.Info("updating stacks...")
-		for _, swarmStack := range stacks {
-			waitGroup.Add(1)
-			go updateStackThread(swarmStack, &waitGroup)
+		stacksChannel := make(chan *swarmStack, len(stacks))
+
+		// Start worker pool
+		logger.Debug(fmt.Sprintf("worker count: %v", config.Concurrency))
+		for range config.Concurrency {
+			go worker(stacksChannel, &waitGroup)
 		}
+
+		// Send stacks to workers
+		for _, swarmStack := range stacks {
+			logger.Debug(fmt.Sprintf("Queueing stack %v for update", swarmStack.name))
+			waitGroup.Add(1)
+			stacksChannel <- swarmStack
+		}
+		close(stacksChannel)
+
+		// Wait for all workers to complete
 		waitGroup.Wait()
+
 		logger.Info("waiting for the update interval")
 		time.Sleep(time.Duration(config.UpdateInterval) * time.Second)
 	}
 }
 
-func updateStackThread(swarmStack *swarmStack, waitGroup *sync.WaitGroup) {
+func worker(stacks <-chan *swarmStack, waitGroup *sync.WaitGroup) {
+	for swarmStack := range stacks {
+		updateStackThread(swarmStack)
+		waitGroup.Done()
+	}
+}
+
+func updateStackThread(swarmStack *swarmStack) {
 	repoLock := swarmStack.repo.lock
 	repoLock.Lock()
 	defer repoLock.Unlock()
-	defer waitGroup.Done()
 
-	logger.Info(fmt.Sprintf("updating %s stack", swarmStack.name))
+	logger.Debug(fmt.Sprintf("%s checking if stack needs to be updated", swarmStack.name))
 	revision, err := swarmStack.updateStack()
 	if err != nil {
 		stackStatus[swarmStack.name].Error = err.Error()
@@ -40,7 +60,7 @@ func updateStackThread(swarmStack *swarmStack, waitGroup *sync.WaitGroup) {
 
 	stackStatus[swarmStack.name].Error = ""
 	stackStatus[swarmStack.name].Revision = revision
-	logger.Info(fmt.Sprintf("done updating %s stack", swarmStack.name))
+	logger.Debug(fmt.Sprintf("%s stack updates check done", swarmStack.name))
 }
 
 func GetStackStatus() map[string]*StackStatus {
